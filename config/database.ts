da124e5b615e3ed24a672ebb29e751c9e33b063a
Ahmed39-lab <AhmedJanWorks@gmail.com>
2026-01-10 18:36:04 +0500

    new changes donwe

--- config/database.ts
+++ config/database.ts
@@ -47,16 +47,15 @@ export default ({ env }) => {
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 0,
+      max: 5,
     },
   },
-    pool: { min: 0, max: 5 },
-  },
+},
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
